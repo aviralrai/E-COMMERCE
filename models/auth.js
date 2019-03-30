ba9a3516c20a25172721f36adc9bdbd73b6752a6
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-30 16:01:13 +0530

    add navbar

--- models/auth.js
+++ models/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require("mysql");
 const db = require("../database");
-const app = express.Router();
+const app = express.Router({mergeParams:true});
 
 app.post("/signup",(req,res)=>{
     const {email,password,fullname} = req.body;
