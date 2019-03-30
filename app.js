5f9e7ccbc3283a77c45bcea1a8cf3a3629d6316a
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-30 10:35:44 +0530

    add initial backend boilerPlate and server signin and signup page

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.port||3000;
+
+
+app.use(express.json());
+app.use(express.static("staticFiles"));
+
+app.get("/",(req,res)=>{
+    res.render("form.ejs");
+})
+
+
+app.listen(port,()=>{
+    console.log(port+" is listening...");
+})
