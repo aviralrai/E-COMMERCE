6338f722e7eb497ff7dbdc7dd7aeba4648077db9
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-30 12:08:19 +0530

    add database connection

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.port||3000;
+var mysql = require('mysql');
+//database
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "shiv",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 
-app.use(express.json());
 app.use(express.static("staticFiles"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("form.ejs");
+})
+
+app.post("/",(req,res)=>{
+    console.log(req.body);
+})
+
+app.get("/home",(req,res)=>{
+    res.send("Hello World");
 })
 
 
