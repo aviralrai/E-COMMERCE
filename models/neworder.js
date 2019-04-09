2aefa23c978aafcdc4057086760e430285de87d0
ced17i026 <ced17i026@iiitdm.ac.in>
2019-04-09 10:26:12 +0530

    make database queries for new orders for retailer

--- models/neworder.js
+++ models/neworder.js
@@ -3,12 +3,13 @@ const db = require("../database");
 const app = express.Router();
 
 app.get('/',(req,res)=>{
-    var query = 'select * from items i inner join orders o on o.sellerid = '+user.userid+' and o.itemid = i.itemid';
+    var query = 'select * from items i inner join orders o on o.itemid = i.itemid and o.status = "pending" and i.sellerid = '+user.userid;
     db.query(query,(err,result)=>{
         if(err){
             console.log(err);
         }else{
-            res.render("neworder.ejs",{data: result});
+            console.log(result);
+            res.render("neworders.ejs",{data: result});
         }
     })
 
